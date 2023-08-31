aa91a50d004110c26cd5043f738775cb1363b1a7
Grace Aliko <aliko@kali.gracealiko.com>
2023-08-31 19:31:04 +0000

    added suggested functions

---
+++ contracts/Donation.sol
@@ -0,0 +1,27 @@
+
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Donation{
+
+
+   // function to donate MATIC
+    function donate(){
+
+    //  add donation amount
+    //  enter purpose
+    // click to donate
+   // increment total donation amount
+   //  update donations mapping
+    }
+
+   // a function to enable you withdraw donated funds
+    function viewTotalDonations(){
+
+    }
+
+    function viewDonerList(){
+
+    }
+}
