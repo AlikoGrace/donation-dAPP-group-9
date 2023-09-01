e7a263ca66f46d836965fbea77afe7cf59e6951d
PaulBless <eshunbless1@gmail.com>
2023-09-01 20:53:47 +0000

    worked on setBeneficiary, viewTotalDonations & viewDonorList functions

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -21,16 +21,45 @@ contract Donation{
     Donation[] public donations;
     mapping(address => bool) public donors;
 
+    uint public totalDonationAmount = 0;
+
     // event to show donation is received
     event DonationReceived(address indexed donor, uint256 amount, string message);
+    // event to allow withdrawal 
 
-    // initialize contract with owner
+
+    /** 
+    SECURITY REQUIREMENT
+    * 1. Access Control : 
+    * A Modifier to allow administrative functions restricted to the Contract Owner
+    * denying donors from modifying contract parameters 
+    */
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Sorry, you are not permitted. Only the Contract Owner");
+        _;
+    }
+
+    /**
+    ADMINISTRATIVE FUNCTION
+    * 2. Emergency Stop
+    * A mechanism for the contract owner/administrator 
+    * to pause or halt all contract operations in case of emergencies or vulnerabilities.
+    */
+    
+
+    // initialize contract with owner/beneficiary
     constructor(address _beneficiary) {
         owner = msg.sender;
         beneficiary = _beneficiary;
     }
 
-    // function to donate MATIC
+
+    /**
+    * Functional Requirement
+    * 1. Donate MATIC
+    * Users must be able to send MATIC to the smart contract's address to make a donation.
+    * Donations can be of any value in MATIC.
+    */
     function donate(string memory _message) external payable {
         //  add donation amount
         //  enter purpose
@@ -56,7 +85,12 @@ contract Donation{
     
     }
 
-   // a function to enable you withdraw donated funds
+
+    /**
+    * Functional Requirement
+    * 3. View Total Donations
+    */
+    // a function to get total donated funds: MATIC
     function viewTotalDonations() external view returns (uint256){
         uint256 total = 0;
         for (uint256 i = 0; i < donations.length; i++) {
@@ -65,7 +99,36 @@ contract Donation{
         return total;
     }
 
-    function viewDonerList() public{
 
+    /**
+    * Functional Requirement
+    * 4. View Donor List
+    */
+    function viewDonerList() external view returns (address[] memory){
+        address[] memory donorList = new address[](donations.length);
+        for (uint256 i = 0; i < donations.length; i++) {
+            donorList[i] = donations[i].donor;
+        }
+        return donorList;
     }
+
+
+    /** 
+    ADMINISTRATIVE FUNCTION
+    1. Set Beneficiary 
+    */
+    function setBeneficiary(address _newBeneficiary) external onlyOwner {
+        beneficiary = _newBeneficiary;
+    }
+    
+    
+    /**
+    * Functional Requirement
+    * 5. Withdraw Funds
+    */
+    function withdraw() external onlyOwner {
+       
+    }
+
+   
 }
